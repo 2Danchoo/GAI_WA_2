de3895f160df5a1279b93339bdbb7b27c18ba577
Lee Bareun <2danchoo@naver.com>
2021-07-12 18:10:17 +0900

    21-07-12 django first project _ class 2 - commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -18,9 +21,18 @@ def hello_world(request) :
 
         return HttpResponseRedirect(reverse('accountapp:hello_world'))  # 새로고침 문제 해결
 
-
-
     else:
         data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list':data_list})
+
+
+class AccountCreateView(CreateView) :
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
+
+
+
